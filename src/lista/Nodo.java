2a25b35013d7922029c7d95deb59ccdb1798a10c
fede-rodriguez <fede3r@gmail.com>
2013-09-01 22:59:28 -0300

    Se agregan generics al Nodo

--- src/lista/Nodo.java
+++ src/lista/Nodo.java
@@ -1,26 +1,33 @@
 package lista;
 
-public class Nodo {
+public class Nodo<TipoDato> {
 
-	private Nodo siguiente;
-	private Dato dato;
+	private Nodo<TipoDato> siguiente;
+	private TipoDato dato;
 	
-	public Nodo siguiente() {return null;}
-	public Dato dato(){return null;}
-
-	public void setSiguiente(Nodo siguiente) {
-		this.siguiente = siguiente;		
-	}
-	
-	public Nodo getSiguiente(){
-		return this.siguiente;
-	}
-
-	public void setDato(Dato dato) {
+	public Nodo(TipoDato dato){
 		this.dato = dato;
 	}
 	
-	public Dato getDato() {
+	public Nodo<TipoDato> siguiente() {
+		return siguiente;
+	}
+	
+	public Dato dato(){return null;}
+
+	public void setSiguiente(Nodo<TipoDato> siguiente) {
+		this.siguiente = siguiente;
+	}
+	
+	public Nodo<TipoDato> getSiguiente(){
+		return this.siguiente;
+	}
+
+	public void setDato(TipoDato dato) {
+		this.dato = dato;
+	}
+	
+	public TipoDato getDato() {
 		return this.dato;
 	}
 }
